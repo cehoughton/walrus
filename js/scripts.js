7086ec6b7f9106ef8a36087018f290561835486a
chris <cehoughton71@gmail.com>
2015-09-30 18:50:07 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(function() {
+$("h1").click(function() {
+  alert("This is a header.");
+  alert("I told you, THIS IS A HEADER!") ;
+});
+
+$("p").click(function() {
+  alert("This is a paragraph.");
+});
+
+$("img").click(function() {
+  alert("This is an image.");
+});
+
+});
+$("h2").click
